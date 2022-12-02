d622ae711d135ea25ca4dbf11891cbf955e8197b
fsenjar <>
2022-12-02 11:35:15 -0700

    UI nearly completed

--- client/src/components/StudentComplains.js
+++ client/src/components/StudentComplains.js
@@ -9,10 +9,10 @@ const user = {
     "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
 };
 const navigation = [
-  { name: "Complains", href: "http://localhost:3000/studentdashboard/complains", current: true },
-  // { name: "Profile", href: "http://localhost:3000/profile", current: false },
-  { name: "Maintenance", href: "http://localhost:3000/studentdashboard/maintenance", current: false },
   { name: "Dashboard", href: "http://localhost:3000/studentdashboard", current: false },
+  { name: "Maintenance", href: "http://localhost:3000/studentdashboard/maintenance", current: false },
+  // { name: "Profile", href: "http://localhost:3000/profile", current: false },
+  { name: "Complains", href: "http://localhost:3000/studentdashboard/complains", current: true },
 ];
 const userNavigation = [
   // { name: "Your Profile", href: "#" },
