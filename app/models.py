d3700701bc58cfa3efd54f1d0775dadacacacd63
RohannChaudhary <118242480+RohannChaudhary@users.noreply.github.com>
2022-12-07 23:21:21 -0700

    final commit

--- app/models.py
+++ app/models.py
@@ -23,6 +23,9 @@ class Person(models.Model):
 class Food_Plan(models.Model):
     type = models.CharField(max_length=20,primary_key=True)
     days = models.IntegerField()
+    
+    def __str__(self):
+        return str(self.type) + " (days:" + str(self.days) + ")"
 
 # Student model
 class Student(models.Model):
@@ -39,26 +42,24 @@ class Student(models.Model):
 # Staff model
 class Staff(models.Model):
     user = models.OneToOneField('Person', primary_key=True, on_delete=models.CASCADE)
-    # def __str__(self):
-    #     return self.user
+    def __str__(self):
+        return self.user
 
 # Technician model
 class Technician(Staff):
     technicianID = models.CharField(max_length=8)
     gender = models.CharField(max_length=30, null=True)
     specialization = models.CharField(max_length=100, null=True)
-    # def __str__(self):
-    #     return str(str(self.user) + '(' + self.specialization + ')')  
+    def __str__(self):
+        return str(str(self.user) + '(' + self.specialization + ')')  
 
 # Admin model
 class Admin(Staff):
     adminID = models.CharField(max_length=8)
     gender = models.CharField(max_length=30,null=True)
     position = models.CharField(max_length=100, null=True)
-    # def __str__(self):
-    #     # list_display = [field.name for field in Admin._meta.get_fields()]
-    #     # return list_display
-    #     return str(self.user) + '(' + self.position + ')'  
+    def __str__(self):
+        return str(self.user) + '(' + self.position + ')'  
 
 
 # Building model
@@ -66,6 +67,9 @@ class Building(models.Model):
     buildingID = models.AutoField(primary_key=True)
     buildingName = models.CharField(max_length=100)
     phoneNo = models.CharField(max_length=10)
+    
+    def __str__(self):
+        return str(self.buildingID) + " (name:" + str(self.buildingName) + ")"
 
 class Room(models.Model):
     roomNo = models.CharField(max_length=6)
@@ -74,12 +78,18 @@ class Room(models.Model):
     class Constraint:
         constraints = [models.UniqueConstraint(fields=['roomNo','buildingID'], name = 'roomID')]
         
+    def __str__(self):
+        return str(self.roomNo) + " (buildingID:" + str(self.buildingID) + ")"
+        
 class Room_Booking(models.Model):
     roomNo = models.ForeignKey('Room',on_delete=models.CASCADE)
     studentID = models.OneToOneField('Student', on_delete=models.SET_NULL,null=True)
 
     class Constraint:
         constraints = [models.UniqueConstraint(fields=['roomNo','studentID'], name = 'roomBookingConstraint')]
+    
+    def __str__(self):
+        return str(self.roomNo) + " (studentID:" + str(self.studentID) + ")"
 
 
 class Maintenance(models.Model):
@@ -96,6 +106,9 @@ class Maintenance(models.Model):
     last_name = models.CharField(max_length=30,default='100')
     details = models.TextField()
     status = models.CharField(max_length=20, choices=STATUS, default='NOT RESOLVED')
+    
+    def __str__(self):
+        return str(self.maintenanceID) + " (studentID:" + str(self.studentID) + ")"
 
 class Complain(models.Model):
     STATUS = (
@@ -112,9 +125,15 @@ class Complain(models.Model):
     first_name = models.CharField(max_length=30,null=True)
     last_name = models.CharField(max_length=30,null=True)
 
+    def __str__(self):
+        return str(self.complainID) + " (studentID:" + str(self.student) + ")"
+    
 class Fulfills_Maintenance(models.Model):
     maintenanceID = models.ForeignKey('Maintenance',on_delete=models.SET_NULL,null=True)
     technicianID = models.ForeignKey('Technician',on_delete= models.SET_NULL,null=True)
+    
+    def __str__(self):
+        return str(self.maintenanceID) + " (technicianID:" + str(self.technicianID) + ")"
     
 class Payment(models.Model):
     paymentID = models.AutoField(primary_key=True)
@@ -122,9 +141,15 @@ class Payment(models.Model):
     studentID = models.OneToOneField('Student',on_delete=models.SET_NULL,null=True)
     amount = models.IntegerField()
     
+    def __str__(self):
+        return str(self.paymentID) + " (studentID: " + str(self.studentID) + " amount: " + str(self.amount) + ")"
+    
     
 class Mail(models.Model):
     mailID = models.AutoField(primary_key=True)
     date = models.DateField()
     studentID = models.ForeignKey(Student,on_delete=models.CASCADE)
     pickedUp = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return str(self.mailID) + " (studentID:" + str(self.studentID) + " pcikedUp: " + str(self.pickedUp) + ")"
